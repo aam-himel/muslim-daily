2d69c2bdacde8920b1da3acc217a1613e5a57e42
aam-himel <aam.himel@gmail.com>
2023-02-08 20:49:11 +0600

    tajbih on progress

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,53 +1,55 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Image, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
-import featuredIcon from '../images/featuredicon.png'
+import featuredIcon from '../images/featuredicon.png';
 
-const renderCardText = (content) => {
-  if(content > 0){
-    return  <Text style={styles.cardText}>{content}</Text>
+const renderCardText = content => {
+  if (content > 0) {
+    return <Text style={styles.cardText}>{content}</Text>;
   }
-  return(
-    <Text style={styles.cardText}>{content}</Text>
-  )
-}
+  return <Text style={styles.cardText}>{content}</Text>;
+};
 
-const Card = ({width,height, round, title, icon, ...props},) => {
+const Card = ({width, height, round, title, icon, ...props}) => {
   const {content} = props;
   return (
-    <View style={[{ width: width, height: height, borderRadius: round, ...props },styles.cotnainer,]}>
-      <Text style={styles.cardTitle}>{ title }</Text>
+    <View
+      style={[
+        {width: width, height: height, borderRadius: round, ...props},
+        styles.cotnainer,
+      ]}>
+      <Text style={styles.cardTitle}>{title}</Text>
       {renderCardText(content)}
-      {icon == false ? null : <View style={styles.cardIcon}>
-        <Image source={featuredIcon} />
-      </View>}
-      
+      {icon == false ? null : (
+        <View style={styles.cardIcon}>
+          <Image source={featuredIcon} />
+        </View>
+      )}
     </View>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
 
 const styles = StyleSheet.create({
-    cotnainer: {
-        backgroundColor: '#714AE8',
-        paddingHorizontal: 24,
-        paddingTop: 24,
-    },
-    cardTitle: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        color: '#FFFFFF',
-    },
-    cardText: {
-        fontSize: 16,
-        marginTop: 12,
-        color: '#FFFFFF',
-    },
-    cardIcon: {
-        position: 'absolute',
-        top: "40%",
-        right: '8%',
-
-    }
-})
+  cotnainer: {
+    backgroundColor: '#714AE8',
+    paddingHorizontal: 24,
+    paddingTop: 24,
+  },
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+  },
+  cardText: {
+    fontSize: 16,
+    marginTop: 12,
+    color: '#FFFFFF',
+  },
+  cardIcon: {
+    position: 'absolute',
+    top: '40%',
+    right: '8%',
+  },
+});
--- app/screens/TasbihScreen.js
+++ app/screens/TasbihScreen.js
@@ -1,9 +1,20 @@
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Vibration,
+} from 'react-native';
 import React, {useEffect, useState} from 'react';
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
 
 const TasbihScreen = () => {
   const [currentCount, setCurrentCount] = useState(0);
   const [totalCount, setTotalCount] = useState(0);
+
+  const HALF_SEC = 500;
 
   useEffect(() => {
     setCurrentCount(0);
@@ -16,15 +27,27 @@ const TasbihScreen = () => {
     } else {
       setCurrentCount(currentCount + 1);
     }
+    Vibration.vibrate(0.5 * HALF_SEC);
     console.log(currentCount);
   };
   return (
     <View style={styles.container}>
       <View style={styles.card}>
-        <Text style={styles.counterText}> {currentCount} / 33</Text>
+        <Text style={styles.counterText}> {currentCount} / 33 </Text>
+        <TouchableOpacity style={styles.edit}>
+          <MaterialIcon name="edit" color="white" size={28} />
+        </TouchableOpacity>
+        <Text style={styles.total}>Total</Text>
       </View>
+
       <TouchableOpacity onPress={handlePress} style={styles.button}>
         <Text style={styles.text}>Count Tajbih</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.icon}>
+        <Icon name="restart" color="white" size={28} />
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.icon2}>
+        <Icon name="vibrate" color="white" size={28} />
       </TouchableOpacity>
     </View>
   );
@@ -36,31 +59,49 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    backgroundColor: '#22c55e',
+    backgroundColor: '#0f172a',
   },
   card: {
-    height: '80%',
+    height: '50%',
     width: '100%',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: '20%',
   },
   counterText: {
     fontSize: 55,
     fontWeight: 'bold',
-    color: 'white',
+    color: '#eab308',
   },
   button: {
-    height: 80,
+    height: 200,
     width: '80%',
     justifyContent: 'center',
-    backgroundColor: '#f8fafc',
+    backgroundColor: '#374151',
     borderRadius: 20,
-    elevation: 1,
   },
   text: {
     fontSize: 16,
     textAlign: 'center',
     textAlignVertical: 'center',
     fontWeight: 'bold',
+    color: '#eab308',
+  },
+  total: {
+    fontSize: 24,
+    paddingTop: 30,
+  },
+  edit: {
+    position: 'relative',
+    top: 0,
+  },
+  icon: {
+    position: 'absolute',
+    bottom: '5%',
+    left: '10%',
+  },
+  icon2: {
+    position: 'absolute',
+    bottom: '5%',
+    right: '10%',
   },
 });
